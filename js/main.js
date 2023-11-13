1489319a9c0928e656e4f30ee9b7c1d1f480a583
CampanelliEmanuele <emanuele.campanelli2001@gmail.com>
2023-11-13 14:57:30 +0100

    Exercise completed

--- js/main.js
+++ js/main.js
@@ -2,17 +2,26 @@ let km = prompt("Enter distance (in km): ");
 let age = prompt("Enter your age: ");
 
 if (km > 0 && age > 0 && age <= 100) {
+    let output = `
+Entered informations:
+${km}km
+${age} years
+
+The price for you is: `;
+
     let basePrice = 0.21 * km;
     let onePercent = basePrice / 100;
     let finalPrice = basePrice;
 
-    if (age >= 18)
-        finalPrice = basePrice - 20 * onePercent;
-    else if (age >= 65)
+    if (age >= 65) {
         finalPrice = basePrice - 40 * onePercent;
+    }
+    else if (age >= 18) {
+        finalPrice = basePrice - 20 * onePercent;
+    }
 
-    console.log("The base price is: " + basePrice);
-
+    output += finalPrice;
+    console.log(output);
 } else if (km == 0 || age == 0) {
     console.log("ERROR: input cannot be equal to 0.");
 } else {
